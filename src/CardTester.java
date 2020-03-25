b72e79a56f863fbae6161cd4f0473a18bf4dac0d
duquema <duquem713@gmail.com>
2020-03-25 11:31:40 -0600

    redid a1 because my mac is messed up

--- src/CardTester.java
+++ src/CardTester.java
@@ -4,10 +4,33 @@
 public class CardTester {
 
 	/**
-	 * The main method in this class checks the Card operations for consistency.
+	 * The main method in this class checks the Card operatins for consistency.
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("ace", "spades", 7);
+		Card card2 = new Card("queen", "hearts", 10);
+		Card card3 = new Card("seven", "diamonds", 4);
+		Card card1Duplicate = new Card("ace", "spades", 7);
+
+		System.out.println(card1.rank());
+		System.out.println(card2.rank());
+		System.out.println(card3.rank());
+		System.out.println(card1.suit());
+		System.out.println(card2.suit());
+		System.out.println(card3.suit());
+		System.out.println(card1.pointValue());
+		System.out.println(card2.pointValue());
+		System.out.println(card3.pointValue());
+		System.out.println(card1.matches(card2));
+		System.out.println(card1.matches(card3));
+		System.out.println(card2.matches(card1));
+		System.out.println(card2.matches(card3));
+		System.out.println(card3.matches(card1));
+		System.out.println(card3.matches(card2));
+		System.out.println(card1.matches(card1Duplicate));
+		System.out.println(card1.toString());
+		System.out.println(card2.toString());
+		System.out.println(card3.toString());
 	}
 }
